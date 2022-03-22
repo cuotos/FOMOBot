18d09501be3e97339e26796d937ea863c7d964f3
Dan Potepa <dan@bloomandwild.com>
2022-03-22 23:58:21 +0000

    it works

--- repository.go
+++ repository.go
@@ -7,18 +7,23 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+// TODO we are not actually using this interface anywhere!
 type Repository interface {
-	Incr(string) error
-	Get(string) (int, error)
+	Incr(context.Context, string) (int, error)
+	Get(context.Context, string) (int, error)
+	Set(context.Context, string, interface{}, time.Duration) error
 }
 
 type RedisRepository struct {
-	rdb *redis.Client
+	rdb                         *redis.Client
+	notificationThresholdPeriod time.Duration
 }
 
-func NewRedisRepository(addr string, password string, db int) (*RedisRepository, error) {
+func NewRedisRepository(addr string, password string, db int, notificationThreshold time.Duration) (Repository, error) {
 
-	repo := &RedisRepository{}
+	repo := &RedisRepository{
+		notificationThresholdPeriod: notificationThreshold,
+	}
 
 	redisClient := redis.NewClient(&redis.Options{
 		Addr:     addr,
@@ -34,16 +39,22 @@ func NewRedisRepository(addr string, password string, db int) (*RedisRepository,
 	return repo, nil
 }
 
-func (repo RedisRepository) Incr(ctx context.Context, key string) (int, error) {
+func (repo *RedisRepository) Incr(ctx context.Context, key string) (int, error) {
 
 	pipe := repo.rdb.TxPipeline()
 	incr := pipe.Incr(ctx, key)
-	pipe.Expire(ctx, key, time.Minute) // this will expire from redis 1 min after the last "increment"
+	// This is will set TTL back to threshold on every reaction, its only wanted on the first one...
+	// or is this ok?? "the counter is reset on every call... if there are none for <timeout> seconds, the redis item disapears..."
+	pipe.Expire(ctx, key, repo.notificationThresholdPeriod)
 
 	_, err := pipe.Exec(ctx)
 	return int(incr.Val()), err
 }
 
-func (repo RedisRepository) Get(key string) (int, error) {
+func (repo *RedisRepository) Get(ctx context.Context, key string) (int, error) {
 	return repo.rdb.Get(context.Background(), key).Int()
+}
+
+func (repo *RedisRepository) Set(ctx context.Context, key string, value interface{}, exp time.Duration) error {
+	return repo.rdb.Set(ctx, key, value, exp).Err()
 }
